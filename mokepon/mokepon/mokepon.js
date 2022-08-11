ef978584a1cabb0871954ef179f6554f1854a35e
Camilo Del Valle Ledesma <camilodelvallel@gmail.com>
2022-08-11 00:42:37 -0500

    Selección de personajes automática en Mokepon

--- mokepon/mokepon/mokepon.js
+++ mokepon/mokepon/mokepon.js
@@ -3,18 +3,42 @@ function iniciarJuego(){
     botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
 }
 
+
 function seleccionarMascotaJugador(){
-    if(document.getElementById("Hipodoge").checked==true){
-        return alert("Elegiste Hipodoge")
+    let inputHipoge=document.getElementById("Hipodoge")
+    let inputCapipepo=document.getElementById("Capipepo")
+    let inputRatigueya=document.getElementById("Ratigueya")
+    let spanMascotaJugador=document.getElementById("mascotaJugador")
+
+    if(inputHipoge.checked==true){
+        spanMascotaJugador.innerHTML="Hipodoge"
+    } else if(inputCapipepo.checked==true){
+        spanMascotaJugador.innerHTML="Capipepo"
+    } else if(inputRatigueya.checked==true){
+        spanMascotaJugador.innerHTML="Ratigueya"
+    } else {
+        return alert("No Elegiste nada")
     }
-    else if(document.getElementById("Capipepo").checked==true){
-        return alert("Elegiste Capipepo")
-    }
-    else {
-        return alert("Elegiste Ratigueya")
-    }
+    
+    seleccionarMascotaEnemigo()
 }
 
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascotaEnemigo")
+    
+    if(ataqueAleatorio==1){
+        spanMascotaEnemigo.innerHTML = "Hipodoge"        
+    } else if (ataqueAleatorio==2){
+        spanMascotaEnemigo.innerHTML = "Capipepo"
+    } else {
+        spanMascotaEnemigo.innerHTML = "Ratigueya"
+    }
 
+}
+
+function aleatorio(min,max){
+    return Math.floor((Math.random() * (max-min+1) + min))
+}
 
 window.addEventListener("load",iniciarJuego)
